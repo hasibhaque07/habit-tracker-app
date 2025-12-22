b6492b63bf3fa11b77f505c4241778a4c77c9251
Hasib <hasibhaque07@gmail.com>
2025-12-22 19:51:35 +0600

    analytics screen changed

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -1,18 +1,10 @@
-import { useAuthStore } from "@/services/authStorage";
-import { router } from "expo-router";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const AnalyticsScreen = () => {
-  const { resetOnboarding } = useAuthStore();
-  const handleFinish = async () => {
-    resetOnboarding();
-    router.replace("/(onboarding)/welcome");
-  };
   return (
     <View className="flex-1 bg-neutral-900 justify-center items-center">
       <Text className=" text-white ">Analytics Screen</Text>
-      <Button title="Unseen" onPress={handleFinish} />
     </View>
   );
 };
